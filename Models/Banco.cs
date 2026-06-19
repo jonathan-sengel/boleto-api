e09e05447257360040082dae19cc25f4bf56c034
Jonathan Engel <joow.engeel@gmail.com>
2026-06-19 20:56:01 -0300

    feat: add Banco endpoints as minimal api

--- Models/Banco.cs
+++ Models/Banco.cs
@@ -2,7 +2,7 @@
 {
     public class Banco
     {
-        public Guid Id { get; set; }
+        public Guid Id { get; set; } = new Guid();
         public string NomeBanco { get; set; }
         public string CodigoBanco { get; set; }
         public decimal PercentualJuros { get; set; }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using BoletoAPI.Data;
+using BoletoAPI.Models;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -15,6 +16,35 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.MapGet("/", context =>
+{
+    context.Response.Redirect("/api/bancos");
+    return Task.CompletedTask;
+});
+
+app.MapGet("/api/bancos", static async (ApplicationDbContext db) => await db.Bancos.ToListAsync());
+
+app.MapGet("/api/bancos/{codigo}",
+    async (string codigo, ApplicationDbContext db) =>
+        {
+            var banco = await db.Bancos
+                .Where(b => b.CodigoBanco.Equals(codigo))
+                .SingleOrDefaultAsync();
+            if (banco is not null) return Results.Ok(banco);
+            return Results.NotFound();
+        })
+    .Produces<Banco>(200)
+    .Produces(404);
+
+app.MapPost("/api/bancos/novo", async (ApplicationDbContext db,
+    Banco banco) =>
+        {
+            db.Bancos.Add(banco);
+            await db.SaveChangesAsync();
+            return Results.Created($"/api/bancos/{banco.Id}", banco);
+        })
+    .Produces<Banco>(201);
 
 app.UseHttpsRedirection();
 app.Run();

c52de153b9db1e21e6d873aa4d2e779b071768dd
Jonathan Engel <joow.engeel@gmail.com>
2026-06-18 21:46:56 -0300

    feat: add models Boleto and Banco

---
+++ Models/Banco.cs
@@ -0,0 +1,10 @@
+﻿namespace BoletoAPI.Models
+{
+    public class Banco
+    {
+        public Guid Id { get; set; }
+        public string NomeBanco { get; set; }
+        public string CodigoBanco { get; set; }
+        public decimal PercentualJuros { get; set; }
+    }
+}
---
+++ Models/Boleto.cs
@@ -0,0 +1,15 @@
+﻿namespace BoletoAPI.Models
+{
+    public class Boleto
+    {
+        public Guid Id { get; set; }
+        public string NomePagador { get; set; }
+        public string CpfCnpjPagador { get; set; }
+        public string NomeBeneficiario { get; set; }
+        public string CpfCnpjBeneficiario { get; set; }
+        public decimal Valor { get; set; }
+        public DateTime DataVencimento { get; set; }
+        public string Observacao { get; set; }
+        public Guid BancoId { get; set; }
+    }
+}
